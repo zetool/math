0ee1897a915e28064d6af33275b856480ac9878b
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2020-10-05 00:15:05 +0200

    Improved documentation

--- src/main/java/org/zetool/math/geom/NDimensional.java
+++ src/main/java/org/zetool/math/geom/NDimensional.java
@@ -13,7 +13,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-
 package org.zetool.math.geom;
 
 /**
@@ -22,30 +21,36 @@ package org.zetool.math.geom;
  * @param <E>
  */
 public interface NDimensional<E extends Number> {
-  /**
-   * Returns the dimension of the {@literal n}-dimensional tuple.
-   * @return 
-   */
-  int getDimension();
-  
-  /**
-   * Returns the {@literal i}th index.
-   * @param i
-   * @return 
-   */
-  E get( int i );
 
-  /**
-   * Returns a representation of the {@literal n}-dimensional tuple in form {@literal (x_1, x_2, ..., x_n)}.
-   * @return string representation as {@literal (x_1, x_2, ..., x_n)}
-   */
-  default String parseableString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append( "(" );
-    for( int i = 0; i < getDimension(); ++i ) {
-      sb.append( get(i) ).append( "," );
+    /**
+     * Returns the dimension of the {@literal n}-dimensional tuple.
+     *
+     * @return
+     */
+    int getDimension();
+
+    /**
+     * Returns the value of a given coordinate {@code index}. The coordinate is in the range {@code 0} to
+     * {@link #getDimension() getDimension() - 1}.
+     *
+     * @param index the index of the value that is to be returned
+     * @return the value of the coordinate with the given {@code index}
+     * @throws IllegalArgumentException if the index is out of range, i.e. negative or larger than the dimension
+     */
+    E get(int index);
+
+    /**
+     * Returns a representation of the {@literal n}-dimensional tuple in form {@literal (x_1, x_2, ..., x_n)}.
+     *
+     * @return string representation as {@literal (x_1, x_2, ..., x_n)}
+     */
+    default String parseableString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("(");
+        for (int i = 0; i < getDimension(); ++i) {
+            sb.append(get(i)).append(",");
+        }
+        sb.replace(sb.length() - 1, sb.length(), ")");
+        return sb.toString();
     }
-    sb.replace(sb.length()-1, sb.length(), ")");
-    return sb.toString();
-  }
 }
